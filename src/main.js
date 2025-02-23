5a5be4c2af71f5d2e3323144f5221056279635e8
Guillermo Eguizabal <cgeguizabal@gmail.com>
2025-02-23 00:31:09 -0600

    Final

---
+++ src/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const displayBtn = document.querySelectorAll(".question-container");
+const icons = document.querySelectorAll(".icon");
+const answer = document.querySelectorAll(".answer-container");
+
+displayBtn.forEach((btn, index) => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const currentIcon = icons[index];
+    const currentAnswer = answer[index];
+
+    currentAnswer.classList.toggle("active");
+    currentAnswer.classList.toggle("deactivated");
+
+    if (currentIcon.src.endsWith("icon-plus.svg")) {
+      currentIcon.src = "./public/icon-minus.svg";
+    } else {
+      currentIcon.src = "./public/icon-plus.svg";
+    }
+  });
+});
